7283cba5680384e79b2f5bad1f245ef9dcda5505
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-24 18:42:34 -0300

    Update proyecto.service.ts

--- Proyecto-Final-AP/Frontend/src/app/service/proyecto.service.ts
+++ Proyecto-Final-AP/Frontend/src/app/service/proyecto.service.ts
@@ -8,7 +8,7 @@ import { Proyecto } from '../model/proyecto.model';
 })
 export class ProyectoService {
 
-  URL = 'http://ypap.up.railway.app/proyecto/';
+  URL = 'https://ypap.up.railway.app/proyecto/';
 
   constructor(private httpClient : HttpClient) { }
 
