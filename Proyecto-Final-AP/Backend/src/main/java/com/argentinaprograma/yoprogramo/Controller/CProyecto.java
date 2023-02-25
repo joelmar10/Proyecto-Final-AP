edb309c2fc94cc6e899a24f3e4f5119e5e52b058
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-25 18:15:56 -0300

    Update CProyecto.java

--- Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CProyecto.java
+++ Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CProyecto.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "**")
+@CrossOrigin(origins = "*")
 public class CProyecto {
     @Autowired
     SProyecto sProyecto;
