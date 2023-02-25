4d335e4774f097f37ac9a81e9a2f7617fd408e9d
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-24 22:57:11 -0300

    CORS

--- Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/YoprogramoApplication.java
+++ Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/YoprogramoApplication.java
@@ -2,6 +2,9 @@ package com.argentinaprograma.yoprogramo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class YoprogramoApplication {
@@ -9,5 +12,18 @@ public class YoprogramoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YoprogramoApplication.class, args);
 	}
+        
+          @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/**").allowedOrigins("https://portafolio-yp.web.app")
+                      .allowedMethods("PUT", "DELETE", "GET", "POST");
+      }
+    };
+  }
 
 }
+
+
