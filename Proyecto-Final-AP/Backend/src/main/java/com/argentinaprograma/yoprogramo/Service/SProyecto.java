c316e54236e883e8eda6effdd9fcb3b79d500d5d
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-24 22:51:16 -0300

    CORS

--- Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Service/SProyecto.java
+++ Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Service/SProyecto.java
@@ -11,9 +11,11 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Service
 @Transactional
+@CrossOrigin(origins = "https://portafolio-yp.web.app")
 public class SProyecto {
      @Autowired
      RProyecto rProyecto;
