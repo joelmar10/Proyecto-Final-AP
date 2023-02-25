215b0acfb0a8c987da523b506631974efb3aef3e
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-25 18:24:45 -0300

    CORS

--- Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CProyecto.java
+++ Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CProyecto.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://portafolio-yp.web.app")
 public class CProyecto {
     @Autowired
     SProyecto sProyecto;
