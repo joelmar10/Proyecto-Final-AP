aee7b9d53e7e08590b83f95aee64616ab57cb622
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-27 15:23:24 -0300

    Update CExperiencia.java

--- Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CExperiencia.java
+++ Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CExperiencia.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"https://portafolio-yp.web.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
