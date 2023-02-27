d737c773ae3619fbde9652762862d1882983653d
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-27 15:23:45 -0300

    Update CHys.java

--- Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CHys.java
+++ Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Controller/CHys.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://portafolio-yp.web.app","http://localhost:4200"})
 @RequestMapping("/skill")
 public class CHys {
 
