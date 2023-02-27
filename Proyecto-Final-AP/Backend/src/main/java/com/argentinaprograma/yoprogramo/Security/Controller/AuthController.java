02898dc61003e394f29f899de3863b1c2b217588
joelmar10 <124640670+joelmar10@users.noreply.github.com>
2023-02-27 15:25:34 -0300

    Update AuthController.java

--- Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Security/Controller/AuthController.java
+++ Proyecto-Final-AP/Backend/src/main/java/com/argentinaprograma/yoprogramo/Security/Controller/AuthController.java
@@ -34,7 +34,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://portafolio-yp.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
